20ac9541ce6c0faa0d168511692a6c402ca42c7e
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-03-23 23:08:33 +0300

    build

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-element'), 'version' => '99ad62ccf9cda8b58d6d');
---
+++ build/index.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";const e=window.wp.element,{render:t,useState:n}=wp.element;t((0,e.createElement)((()=>{const[t,l]=n(0);return(0,e.createElement)("div",null,(0,e.createElement)("h2",null,t," Votes"),(0,e.createElement)("p",null,(0,e.createElement)("button",{onClick:()=>{l(t+1)}},"Vote!")))}),null),document.getElementById("react-app"))})();
